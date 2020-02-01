0b1836295456d58788402a40fe5967a420b080af
Sushil Dangi <dangi.sushil5@gmail.com>
2020-02-01 12:05:24 +0530

    first commit

---
+++ spring-boot-ecommerce/src/main/java/com/luv2tech/SpringBootECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.luv2tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootECommerceApplication.class, args);
+	}
+
+}
---
+++ spring-boot-ecommerce/src/test/java/com/luv2tech/SpringBootECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
