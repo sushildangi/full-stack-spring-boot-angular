52b0ee5c273d950b2aa4387c4f2dd41a7398920b
Sushil Dangi <dangi.sushil5@gmail.com>
2020-04-16 00:49:26 +0530

    Product Listing from api is done

--- spring-boot-ecommerce/src/main/java/com/luv2tech/repository/ProductCategoryRepository.java
+++ spring-boot-ecommerce/src/main/java/com/luv2tech/repository/ProductCategoryRepository.java
@@ -4,7 +4,9 @@ import com.luv2tech.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @Repository
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
--- spring-boot-ecommerce/src/main/java/com/luv2tech/repository/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/luv2tech/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.luv2tech.repository;
 import com.luv2tech.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
