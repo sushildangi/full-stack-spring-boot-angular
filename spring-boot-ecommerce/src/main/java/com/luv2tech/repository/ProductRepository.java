cc2b82aecd15fc99574a6543f3a2b3eb43dec0b7
Sushil Dangi <dangi.sushil5@gmail.com>
2020-04-17 00:40:01 +0530

    Angular router setup and findByCategoryId added in spring boot jpa repository

--- spring-boot-ecommerce/src/main/java/com/luv2tech/repository/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/luv2tech/repository/ProductRepository.java
@@ -1,11 +1,17 @@
 package com.luv2tech.repository;
 
 import com.luv2tech.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id,
+                                   Pageable pageable);
 }
